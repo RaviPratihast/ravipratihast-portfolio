f4deacae2a3decbbadf07171ade7cec54d800042
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-15 21:13:24 +0530

    add: about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Hero from "./component/Hero-section/Hero";
+import About from "./component/About-me/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
